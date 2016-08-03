2ba29339f7835e46f4dd48dc8b44a9026332219b
thomasgriffinx <xytomg@gmail.com>
2016-08-03 10:21:39 +0100

    master

---
+++ developers.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>OpenShift by IIHS</title>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="overview.html">Overview</a></li>
+        <li><a href="developers.html">Developers</a></li>
+        <li><a href="addons.html">Addons</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
+    </div>
+  </nav>
+
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center white-text">Lilly Devs</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">On Prem PaaS by GICOE</h5>
+        </div>
+        <div class="row center">
+          <a href="https://developers.openshift.com/index.html" id="download-button" class="btn-large waves-effect waves-light red lighten-1">Get Started</a>
+        </div>
+        <br><br>
+      </div>
+    </div>
+    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+
+  </body>
+
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  <?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+ // $db = pg_connect('dbname=d81a53c7aurso7 host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com port=5432 user=qgljwvgpvqdmos password=4WgnfX2SVmyfod0LzIrLzQMh9C sslmode=require');
+
+        //$Something = pg_escape_string($_POST['Something']);
+       
+       // $TeamNumber = pg_escape_string($_POST['TeamNumber']);
+
+      //  $query = "INSERT INTO random (Something) VALUES('" . $Something. "')";
+  //  $result = pg_query($query); 
+      //  if (!$result) { 
+          //  $errormessage = pg_last_error(); 
+         //   echo "Nothing here yet but soon im gonna display all the data from your populated PSQL database " . $errormessage; 
+           // exit(); 
+       // } 
+        ?> 
+</html>
---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>OpenShift by IIHS</title>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="overview.html">Overview</a></li>
+        <li><a href="developers.html">Developers</a></li>
+        <li><a href="addons.html">Addons</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center white-text">OpenShift</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">On Prem PaaS by GICOE</h5>
+        </div>
+        <div class="row center">
+          <a href="https://developers.openshift.com/index.html" id="download-button" class="btn-large waves-effect waves-light red lighten-1">Get Started</a>
+        </div>
+        <br><br>
+      </div>
+    </div>
+    <div class="parallax"><img src="background4.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+
+        <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">PaaS</h5>
+            <p class="light">Deploy your applications from Github in Seconds</p>
+          </div>
+        </div>
+     
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">User Driven Service</h5>
+
+            <p class="light">Tell us what you would like to see in upcoming OpenShift releases, from addons to images we want to provide you and your developers with what you need</p>
+          </div>
+        </div>
+
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Easy to work with</h5>
+
+            <p class="light">OpenShift provides a modern container based hosting solution; allowing developers to focus on their application coding.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <br> </br>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center white-text">Developers</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Looking to get started?</h5>
+        </div>
+        <div class="row center">
+          <a href="https://developers.openshift.com/getting-started" id="download-button" class="btn-large waves-effect waves-light blue lighten-1">Developer Guides</a>
+        </div>
+        <br><br>
+      </div>
+    </div>
+    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+<br> </br>
+<p class="light">
+OpenShift provides a modern container based hosting solution; allowing developers to focus on their application code.  OpenShift is RedHat's cloud development Platform as a Service (PaaS). The free and open source cloud-based platform allows developers to create, test and run their applications and deploy them to the cloud - IIHS has deployed an image of OpenShift on Lilly's internal network, providing you with direct access to Lilly's vast IT ecosystem
+</p>
+<br> </br>
+</div>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+
+
+  <?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+ // $db = pg_connect('dbname=d81a53c7aurso7 host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com port=5432 user=qgljwvgpvqdmos password=4WgnfX2SVmyfod0LzIrLzQMh9C sslmode=require');
+
+        //$Something = pg_escape_string($_POST['Something']);
+       
+       // $TeamNumber = pg_escape_string($_POST['TeamNumber']);
+
+      //  $query = "INSERT INTO random (Something) VALUES('" . $Something. "')";
+ 	//	$result = pg_query($query); 
+      //  if (!$result) { 
+          //  $errormessage = pg_last_error(); 
+         //   echo "Nothing here yet but soon im gonna display all the data from your populated PSQL database " . $errormessage; 
+           // exit(); 
+       // } 
+        ?> 
+</html>
